a38260ef858a6acedbdd7c69432eeb71177509f7
Christy Perozzi <christy.perozzi@gmail.com>
2022-01-05 21:40:22 -0500

    commit tree 760b6848f3bb8cb3810807960bb11e43560fa673

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,60 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  // All possible tasks from data and user-submitted
+  const [allTasks, setAllTasks] = useState(TASKS)
+  //filtered tasks 
+  const [tasksToDisplay, setTasksToDisplay] = useState(allTasks)
+  const [newDetails, setNewDetails] = useState("");
+  const [newCat, setNewCat] = useState("Code")
+
+  function handleButton(event){
+    event.target.parentNode.childNodes.forEach(x => x.className = "");
+    event.target.className = "selected"; 
+    setTasksToDisplay( allTasks.filter( (task) => {
+      if (event.target.textContent === "All") {
+        return true;
+      } 
+      return task.category === event.target.textContent } ))
+  }
+
+  function handleTaskFormSubmit (event){
+    event.preventDefault(); 
+    const newTask = {
+      text: newDetails,
+      category: newCat,
+    }
+    
+    setTasksToDisplay([...tasksToDisplay, newTask]) 
+    setAllTasks([...allTasks, newTask]) 
+    
+    setNewCat("Code"); 
+    setNewDetails("");
+    
+  }
+
+  function handleDetailsChange(event){
+    setNewDetails(event.target.value);
+    console.log(newDetails)
+  }
+
+  function handleCatChange(event){
+    setNewCat(event.target.value);
+    console.log(newCat)
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} onButtonClick ={handleButton} />
+      <NewTaskForm  categories={CATEGORIES} 
+                    newCat={newCat} 
+                    newDetails={newDetails} 
+                    onTaskFormSubmit = {handleTaskFormSubmit}
+                    onDetailsChange = {handleDetailsChange}
+                    onCatChange = {handleCatChange} />
+      <TaskList tasks = {tasksToDisplay}/>
     </div>
   );
 }
